81c31a23cc36af853ab037af5eb83d62cdf84cfd
CodeVishal <vishal.pawar57@gmail.com>
2019-10-07 22:53:07 +0530

    First Commit

---
+++ HelloExam.java
@@ -0,0 +1,12 @@
+public class HelloExam{
+	
+	public static void main(String args){
+		System.out.println("Hello Exam 2");
+	}
+	public static void main(String [] args){
+		System.out.println("Hello Exam");
+	}
+	public static void main(int number){
+		System.out.println("Hello Exam 3")
+	}
+}
